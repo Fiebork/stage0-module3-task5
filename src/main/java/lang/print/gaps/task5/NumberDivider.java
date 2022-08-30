b5d762e3bb66f6f670576a4b2ead1752ac1f800b
Bilbo3340 <vladik.elce1@gmail.com>
2022-08-30 17:45:45 +0300

    upt

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        double a = 19;
+        System.out.println(a / 5);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        double a = 11.5;
+        System.out.println(a * 60);
     }
 }
